f7a3a22abc79e45b181d115f89066e1ea0a582d1
Alec Morgan <alec.morgan@berkeley.edu>
2023-05-25 13:12:03 -0700

    Added clear button and started backend for clearing data and solving functions

--- functions.js
+++ functions.js
@@ -26,3 +26,16 @@ function operate(operator, num1, num2) {
     }
     return null;
 }
+
+function clear() {
+    // reset
+}
+
+//on button click, one of [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], update display with new number.
+// first number, then just add to right of it. so treat display nums like appending string
+
+//num1 == numbers pressed before operator?
+// once operator pressed (multiply, divide, sub, add), then start registering buttons pressed into next variable
+//num2 == numbers pressed after operator
+
+//then do operate() from these given parameters when equals is pressed
