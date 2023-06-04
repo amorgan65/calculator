13a1a65c347482236b2439467d46378253081465
Alec Morgan <alec.morgan@berkeley.edu>
2023-06-03 17:57:18 -0700

    Added event listeners for operator buttons so all operations can be used

--- functions.js
+++ functions.js
@@ -1,26 +1,17 @@
-//set buttons to have listener event?
 var num1 = ''
 var num2 = ''
-var operator = null
-
+var operator = ''
+var currNumber = '0'
 let firstTerm = true
 
-var currNumber = '0'
-
-
 var numButtons = document.querySelectorAll('button[data-value]')
+var operatorButtons = document.querySelectorAll('button[data-operator]')
 var equalButton = document.getElementById('equals')
-const subButton = document.getElementById('subtract')
-var addButton = document.getElementById('add')
-const multButton = document.getElementById('multiply')
-const divButton = document.getElementById('divide')
-
 var clearButton = document.getElementById('clear')
-//var oneButton = document.getElementById('1')
 var display = document.getElementById('display')
 
+/* Assigns listener for click event to each number button [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] */
 numButtons.forEach((button) => {
-    //TODO attach listener to each?
     button.addEventListener('click', () => numberPressed(button.getAttribute('id')))
 })
 
@@ -31,35 +22,23 @@ function numberPressed(num) {
     }
 
     if (currNumber == '0') {
-        currNumber = num //TODO generalize, change to string value of whatever number button being pressed
+        currNumber = num
         refreshDisplay()
     } else {
-        currNumber += num //TODO generalize, change to string value of whatever number button being pressed
+        currNumber += num
         refreshDisplay()
     }
 }
 
-/*
-//TODO Generalize this function to use ID of whichever button is of class number? to find what number it actually is
-function numOnePressed() {
-    if (firstTerm == false) {
-        currNumber = '0'
-    }
+/* Assigns listener for click event to each operator button [+, -, *, /] */
+operatorButtons.forEach((button) => {
+    button.addEventListener('click', () => operatorPressed(button.getAttribute('id')))
+})
 
-    if (currNumber == '0') {
-        currNumber = '1' //TODO generalize, change to string value of whatever number button being pressed
-        refreshDisplay()
-    } else {
-        currNumber += '1' //TODO generalize, change to string value of whatever number button being pressed
-        refreshDisplay()
-    }
-} */
-
-//TODO generalize function to all operator buttons?
-function addPressed() {
+function operatorPressed(oper) {
     num1 = currNumber 
     currNumber = '0'
-    operator = '+' //TODO operator one of either ['/', '*', '-', '+']
+    operator = oper
     firstTerm = false
 }
 
@@ -71,19 +50,14 @@ function clear() {
     refreshDisplay()
 }
 
+/* Updates div with class display to display the current number */
 function refreshDisplay() {
     display.innerText = currNumber
 }
 
 clearButton.addEventListener('click', clear)
 
-//TODO create listener events for number buttons
-
-addButton.addEventListener('click', addPressed)
-
 equalButton.addEventListener('click', solve)
-
-//TODO create listener events for operator buttons
 
 function add(a, b) {
     return a + b;
@@ -117,12 +91,11 @@ function operate(term, n1, n2) {
     return null;
 }
 
+/* Function called when equal button is pressed */
 function solve() {
     num2 = currNumber
     let result = operate(operator, num1, num2);
-    //TODO num1 = CONVERT STRING TO INT!!!
     let asString = result.toString()
-    //TODO need to fix? should be using currNumber variable, and change display to the result instead?
     currNumber = asString
     refreshDisplay();
     firstTerm = true
@@ -131,12 +104,3 @@ function solve() {
     operator = null
     currNumber = '0'
 }
-
-//on button click, one of [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], update display with new number.
-// first number, then just add to right of it. so treat display nums like appending string
-
-//num1 == numbers pressed before operator?
-// once operator pressed (multiply, divide, sub, add), then start registering buttons pressed into next variable
-//num2 == numbers pressed after operator
-
-//then do operate() from these given parameters when equals is pressed
